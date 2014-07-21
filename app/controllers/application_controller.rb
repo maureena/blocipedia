ba2ac9944a55d147fd693f9cada3b8e99770ca2c
vagrant <vagrant@rails-dev-box>
2014-07-21 04:42:12 +0000

    Configured app for Production emails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  rescue_from Pundit::NotAuthorizedError do |exception|
+  rescue_from 'Pundit::NotAuthorizedError' do |exception|
     redirect_to root_url, alert: "You must be logged in or a paid subsrciber to perform this action."
   end
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Blocipedia::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+   # http://bloccit.herokuapp.com
+   config.action_mailer.default_url_options = { host: 'maureena-blocipedia.herokuapp.com' }
 end
