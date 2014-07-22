d4ecb67ba5d3bf78e57fd00215e572b495eab9a8
vagrant <vagrant@rails-dev-box>
2014-07-22 06:52:20 +0000

    Fixed page policy to restrict access to private pages.

--- app/policies/page_policy.rb
+++ app/policies/page_policy.rb
@@ -10,7 +10,7 @@ class PagePolicy < ApplicationPolicy
 
      def resolve
        if ( user.role?('paid') )
-         scope
+         scope.all
        else
          scope.where(public: true)
        end
@@ -22,9 +22,7 @@ class PagePolicy < ApplicationPolicy
   end
 
   def show?
-    return true if ( user.role?('paid') && @page.editors.include?(user))
-    return true if record.public? || user.present? 
-    false
+    ( user.role?('paid') && record.editors.include?(user) ) || ( record.public? )
   end
   
 end
